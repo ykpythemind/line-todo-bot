1efbad64477d854c57415fdcbf48cd1a1d048163
ykpythemind <yukibukiyou@gmail.com>
2018-04-01 22:30:13 +0900

    reply task

--- app/models/reply_handler.rb
+++ app/models/reply_handler.rb
@@ -1,25 +1,44 @@
 class ReplyHandler
-  attr_reader :text, :reply
+  attr_reader :message, :reply
 
-  def initialize(text, reply_obj)
-    @text = text
+  def initialize(message, reply_obj)
+    @message = message
     @reply = reply_obj
   end
 
   def detect!
-    if text.include? "タスク追加"
+    if message.include? "タスク追加"
+      @message.remove! "タスク追加"
       add_to_db
-    elsif text.include? "タスク"
+    elsif message.include? "タスク完了"
+      @message.remove! "タスク完了"
+      task_done
+    elsif message.include?("タスク") || message.upcase.include?("TASK")
       reply_all_task
     end
   end
 
   private
 
+  def task_done
+    task = Task.find_by(id: message.strip.to_i)
+    if task.present?
+      s = task.text
+      task.destroy
+      reply.add "#{s} をやったぞ"
+    end
+  end
+
   def add_to_db
+    Task.create(text: message)
   end
 
   def reply_all_task
-    reply.add(text)
+    task = Task.latest
+    if task.present?
+      reply.add(task)
+    else
+      reply.add("すべてのタスクが終わってるよ")
+    end
   end
 end
--- app/models/task.rb
+++ app/models/task.rb
@@ -1,2 +1,6 @@
 class Task < ApplicationRecord
+  def self.latest
+    tasks = Task.take(3)
+    tasks.map { |t| "[#{t.id}] #{t.text}" }.join(' | ')
+  end
 end
