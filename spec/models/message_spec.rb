7e584d1e363e8ab765a42205919b541a3fd4d445
Yukito Ito <yukibukiyou@gmail.com>
2018-04-06 23:42:31 +0900

    fix

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,20 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Message, type: :model do
-  describe "通常のメッセージ" do
-    let(:text) { "piyopiyo" }
-    it "does not detect something" do
-      Message.detect!(text) do |result|
-        expect(result).to be_nil
-      end
+
+  describe "detect message" do
+    subject { Message.detect!(text) }
+    describe "通常のメッセージ" do
+      let(:text) { "piyopiyo" }
+      it { is_expected.to be_nil }
+    end
+    describe "タスクメッセージ" do
+      let(:text) { "タスク" }
+      it { is_expected.to eq :all }
     end
   end
-  describe "タスクメッセージ" do
-    let(:text) { "タスク" }
-    it "detect something" do
-      Message.detect!(text) do |result|
-        expect(result).to_not be_nil
-      end
-    end
-  end
+
 end
